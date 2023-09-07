d61db803cc7c1cf5571a308d28facc66ed670ea8
Vinicius Pedroso <vpedroso@wiley.com>
2023-09-07 11:00:35 -0300

    third-wfl setup

---
+++ source/count_files_write_logs.py
@@ -0,0 +1,16 @@
+import logging, os, os.path
+
+DIR = '.'
+
+file_count: int = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
+logging.basicConfig(
+    filename="source/logs.txt",
+    format="%(asctime)s : %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S",
+    level=logging.DEBUG,
+)
+
+logging.info(file_count)
+
+print(f"there are {file_count} files in source folder")
