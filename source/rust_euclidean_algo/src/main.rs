be3d2b4e4f28685d5a6246cb6c27fe3daff85dec
Vinicius Pedroso <vpedroso@wiley.com>
2023-09-14 09:09:02 -0300

    creating rust project with euclidean algo

---
+++ source/rust_euclidean_algo/src/main.rs
@@ -0,0 +1,32 @@
+use std::io::Write;
+use std::str::FromStr;
+
+fn main() {
+    let mut numbers = Vec::new();
+    for arg in std::env::args().skip(1) { 
+        numbers.push(u64::from_str(&arg) 
+        .expect("error parsing argument"));
+    }
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: NO NUMBER INSERTED...").unwrap(); 
+        std::process::exit(1); 
+        }
+    let mut d = numbers[0]; 
+    for m in &numbers[1..] 
+        {d = gcd(d, *m);}
+    
+    println!("The greatest common divisor of {:?} is {}",numbers, d);
+} 
+
+fn gcd(mut x: u64, mut y: u64) -> u64 {
+    assert!(x != 0 &&  y != 0);
+    while y != 0 { 
+        if y < x {
+            let inv = y; 
+            y = x;
+            x = inv;
+        }
+        y = y % x; 
+    }
+    x 
+}
